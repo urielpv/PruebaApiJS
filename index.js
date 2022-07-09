7a3edb97ce789a45a00cd8024bf73dade163c304
Uriel Pelcastre Vargas <pelcastre203@gmail.com>
2022-07-08 23:15:07 -0500

    cambio de api

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 
-const url='https://api.datos.gob.mx/v1';
-
-const ul=document.createElement('ul');
-
-//htmlres=document.querySelector("#app");
- fetch(`${url}/calidadAire`)
- .then((response)=>response.json()
-
-//  .then((calidadAire)=>{
-//     calidadAire.forEach(calidad => {
-        
-//         let elemento=document.createElement("li");
-//         document.createTextNode(`$calidad.stations`);
-//     });
-//  })
+const apiButton=document.getElementById('apiButton');
+const apiData=document.getElementById('apiData');
+const typess=document.getElementById('type');
+const info=document.getElementById('info');
+const ataque=document.getElementById('ataque');
+const defensa=document.getElementById('defensa');
+const raresa=document.getElementById('raresa');
+const atributo=document.getElementById('atributo');
+const arquetipo=document.getElementById('arquetipo');
 
 
- .then(json=>console.log(json))
- 
-);
+const callAPI=()=>{
+    fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark Magician')
+    .then(res=> res.json())
+      .then(data=>{
+          apiData.innerText=`Nombre:${JSON.stringify(data.data[0].name)}`
+          typess.innerText=`Tipo: ${JSON.stringify(data.data[0].type)}`
+      info.innerText=`Descripcion: ${JSON.stringify(data.data[0].desc)}`
+       ataque.innerText=`Ataque: ${JSON.stringify(data.data[0].atk)}`
+       defensa.innerText=`Defensa: ${JSON.stringify(data.data[0].def)}`
+       raresa.innerText=`Raresa: ${JSON.stringify(data.data[0].race)}`
+      atributo.innerText=`Atributo: ${JSON.stringify(data.data[0].attribute)}`
+       arquetipo.innerText=`Arquetipo: ${JSON.stringify(data.data[0].archetype)}`
+        })
+    //.then(data=>console.log(data.data[0].archetype))
+    .catch(e=>console.error(new Error(e)))
+       // console.log(res);
+       
+    
+}
+apiButton.addEventListener('click', callAPI);
